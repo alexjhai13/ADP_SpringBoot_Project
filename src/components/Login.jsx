b01308762865b6608c592d924687b7d89da2be35
Rayyan Sarkhot <rayyanthecrayon@gmail.com>
2025-09-05 16:09:03 -0400

    trial

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,62 +1,84 @@
-import React from 'react'
-import { useState, useEffect } from 'react'
-import NavigationBar from './NavigationBar'
-import { useNavigate } from 'react-router-dom'
-import './Login.css'
+import React from "react";
+import { useState, useEffect } from "react";
+import NavigationBar from "./NavigationBar";
+import { useNavigate } from "react-router-dom";
+import "./Login.css";
 // import { useCookies } from 'react-cookie'
-import * as usersApi from '../api/users';
+import * as usersApi from "../api/users";
 
-const Login = () => {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    //const [cookies, setCookie] = useCookies(['admin']);
-    const [users, setUsers] = useState([]);
-    const navigate = useNavigate();
+//sends headers to /token endpoint to receive JWT
+function validateLoginInfo(username, password, cb) {
+  const myHeaders = new Headers();
+  myHeaders.append("Authorization", `Basic ${btoa(`${username}:${password}`)}`);
 
-    useEffect(() => {
-        usersApi.getAll().then(setUsers);
-    }, []);
+  const requestOptions = {
+    method: "POST",
+    headers: myHeaders,
+    redirect: "follow",
+  };
 
-    const handleLogin = () => {
-        const user = users.find((u)=> username === u.user && password === u.password )
-        //console.log(users[0].user, users[0].password)
-        console.log(username, password) 
-        if (user) {
-            sessionStorage.setItem('isAdmin', 'true');
-            console.log('User logged in successfully');
-            // setCookie(`admin`, true, { path: '/', maxAge: 3600, httpOnly: false });
-            navigate('/dashboard');
-        } else {
-            alert('Invalid username or password');
-        }
-    }
-
-    return (
-        <>
-            <NavigationBar />
-            <div className={'login-container'}>
-                <h1 className={'login-header'}>Login</h1>
-                <input type='text'
-                    name='username'
-                    placeholder='username'
-                    onChange={(e) => setUsername(e.target.value)}
-                    className='login-input'
-                    autoComplete='off'
-                />
-                <div />
-                <input type='text'
-                    name='password'
-                    placeholder='password'
-                    onChange={(e) => setPassword(e.target.value)}
-                    className='login-input'
-                    autoComplete='off'
-                />
-                <br />
-
-                <button onClick={handleLogin} className={'login-button'}>Login</button>
-            </div>
-        </>
-    )
+  fetch(import.meta.env.VITE_REACT_APP_AUTH_SERVER_URL, requestOptions)
+    .then((response) => response.text())
+    .then((result) => {
+      cb(null, result);
+    })
+    .catch((error) => console.error(error));
 }
 
-export default Login
+const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [JWT, setJWT] = useState("");
+  //const [cookies, setCookie] = useCookies(['admin']);
+  const [users, setUsers] = useState([]);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    usersApi.getAll().then(setUsers);
+  }, []);
+
+  const handleLogin = () => {
+    const cb = (error, result) => {
+      if (error) {
+        throw new Error("ValidateLoginInfo had an error.");
+      }
+      setJWT(result);
+    };
+
+    validateLoginInfo(username, password, cb);
+  };
+
+  return (
+    <>
+      <NavigationBar />
+      <div className={"login-container"}>
+        <h1 className={"login-header"}>Login</h1>
+        <input
+          type="text"
+          name="username"
+          placeholder="username"
+          onChange={(e) => setUsername(e.target.value)}
+          className="login-input"
+          autoComplete="off"
+        />
+        <div />
+        <input
+          type="password"
+          name="password"
+          placeholder="password"
+          onChange={(e) => setPassword(e.target.value)}
+          className="login-input"
+          autoComplete="off"
+        />
+        <br />
+
+        <button onClick={handleLogin} className={"login-button"}>
+          Login
+        </button>
+        <h1>jwt: {JWT}</h1>
+      </div>
+    </>
+  );
+};
+
+export default Login;
